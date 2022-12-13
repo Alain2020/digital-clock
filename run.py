db76b3e7891c40adbc4acf3ce97c88d032ba4619
Alain2020 <favouralain@yahoo.com>
2022-12-13 23:40:51 +0000

    Add root and title

--- run.py
+++ run.py
@@ -2,5 +2,12 @@ from tkinter import *
 import tkinter
 
 #define some colors
-corl = "3d3d3d" # black
+corl = "#3d3d3d" # black
 cor2 = "#21c25c" # green 
+
+root = Tk()
+root.title("")
+root.geometry('350x180')
+root.configure(background=corl)
+
+root.mainloop()
